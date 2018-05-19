44fcc8e09f4d5bb0108be2164816466553bf5ddb
Антон Токарчук <mm@MacBook-Air-Anton.local>
2018-05-19 16:49:00 +0300

    Initial commit

---
+++ ObjectDetection/ObjectDetection/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ObjectDetection.Models;
+
+namespace ObjectDetection.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ ObjectDetection/ObjectDetection/Views/ItemsPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using ObjectDetection.Models;
+using ObjectDetection.Views;
+using ObjectDetection.ViewModels;
+
+namespace ObjectDetection.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemsPage : ContentPage
+    {
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+       /* async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            
+        }*/
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
